b86c1bed410990f7eb0fcb230fb92abb7bf23591
avehghasemian <av.ghasemian@gmail.com>
2021-02-04 10:23:27 +0100

    mongoose.model

--- models/User.js
+++ models/User.js
@@ -2,4 +2,11 @@ const mongoose = require('mongoose')
 
 const uniqueValidator = require('mongoose-unique-validator') //Plugin Mongoose nécessaire dans le cas d'un champ unique
 
-const userSchema = 
+const userSchema = mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+})
+
+userSchema.plugin(uniqueValidator) // On lui passe le plugin avant d'exporter le modèle
+
+module.exports = mongoose.model('User', userSchema)
