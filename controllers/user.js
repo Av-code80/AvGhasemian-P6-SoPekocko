0dd6448d1ceb2f3e4d0872b5df6dcd5b436e6302
avehghasemian <av.ghasemian@gmail.com>
2021-02-04 09:53:51 +0100

    exports.signup

--- controllers/user.js
+++ controllers/user.js
@@ -5,5 +5,19 @@ const jwt = require('jsonwebtoken') // On fait appel à JsonWebToken pour attrib
 const User = require('../models/User') // On fait appel à notre modèle 'User'
 const config = require('../config/auth.config') // On récupère la clé pour le TOKEN
 
-
+exports.signup = (req, res, next) => {
+  // Pour la création d'un nouvel utilisateur
+  bcrypt
+    .hash(req.body.password, 10) // Le chiffre '10' indique le nombre de salage du MDP
+    .then((hash) => {
+      const user = new User({
+        email: req.body.email,
+        password: hash,
+      })
+      user
+        .save()
+        .then(() => res.status(201).json({ message: 'Utilisateur créé ! ' }))
+        .catch((error) => res.status(400).json({ error }))
+    })
+    .catch((error) => res.status(500).json({ error }))
 }
