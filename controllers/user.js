3f7503325bd7b2b75ceff991053f99b96d38a5e4
avehghasemian <av.ghasemian@gmail.com>
2021-02-04 10:32:06 +0100

    hash

--- controllers/user.js
+++ controllers/user.js
@@ -14,19 +14,38 @@ exports.signup = (req, res, next) => {
         email: req.body.email,
         password: hash,
       })
-     
       user
-      .save()
-      .then(() => res.status(201).json({ message: 'Utilisateur créé ! ' }))
-      .catch((error) => res.status(400).json({ error }))
-  })
-  .catch((error) => res.status(500).json({ error }))
+        .save()
+        .then(() => res.status(201).json({ message: 'Utilisateur créé ! ' }))
+        .catch((error) => res.status(400).json({ error }))
+    })
+    .catch((error) => res.status(500).json({ error }))
 }
 
 exports.login = (req, res, next) => {
-// Pour la connection à son compte utilisateur
-User.findOne({ email: req.body.email })
-  .then((user) => {
-    if (!user) {
-      return res.status(401).json({ error: 'Utilisateur non trouvé !' })
-    }
+  // Pour la connection à son compte utilisateur
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({ error: 'Utilisateur non trouvé !' })
+      }
+      
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res.status(401).json({ error: 'Mot de passe incorrect !' })
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign(
+              { userId: user._id }, // le payload du TOKEN, userdID nécéssaire dans le cas où une requête transmettrait un userId (création, modification...)
+              config.secret, // Clé d'encodage
+              { expiresIn: '24h' } // Date d'expiration du TOKEN 24h
+            ),
+          })
+        })
+        .catch((error) => res.status(500).json({ error }))
+    })
+    .catch((error) => res.status(500).json({ error }))
+}
