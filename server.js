c1bd40c1b7577ea908e25f398c3e3cb076f67ea8
avehghasemian <av.ghasemian@gmail.com>
2021-02-04 10:16:23 +0100

    serverAdresse

--- server.js
+++ server.js
@@ -22,3 +22,20 @@ const errorHandler = (error) => {
   if (error.syscall !== 'listen') {
     throw error
   }
+
+
+  const address = server.address()
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.')
+      process.exit(1)
+      break
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.')
+      process.exit(1)
+      break
+    default:
+      throw error
+  }
+}
