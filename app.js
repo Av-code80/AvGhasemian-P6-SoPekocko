bc2a896d46e3c860f70f5d6880a0b6a8bbae7fa4
avehghasemian <av.ghasemian@gmail.com>
2021-02-04 10:06:13 +0100

    mongoose

--- app.js
+++ app.js
@@ -9,34 +9,4 @@ const path = require ('path') // Pour le middleware express static pour acceder 
 const sauceRoutes = require('./routes/sauce')
 const userRoutes = require('./routes/user')
 
-mongoose
-  .connect(
-    `mongodb+srv://${mongooseConfig.id}:${mongooseConfig.pwd}@cluster0-uzjno.mongodb.net/piquante?retryWrites=true&w=majority`,
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'))
 
-const app = express()
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*') // On donne l'accès à toute origine '*'
-  res.setHeader(
-    'Access-Control-Allow-Headers', // On donne l'autorisation d'utiliser ces headers sur l'objet réponse
-    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
-  )
-  res.setHeader(
-    'Access-Control-Allow-Methods', // On donne l'autorisation d'utiliser ces actions aux réponse
-    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
-  )
-  next()
-})
-
-app.use(bodyParser.json()) 
-
-app.use('/images', express.static(path.join(__dirname, 'images'))) //Va permettre à l'app de servir le dossier contenant les images, pour le middleware multer
-
-app.use('/api/sauces', sauceRoutes)
-app.use('/api/auth', userRoutes)
-
-module.exports = app // L'application est exporté pour être 'servi' par le serveur
