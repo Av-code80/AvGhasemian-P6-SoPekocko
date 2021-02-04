bd7469f3a1623e58e187071fd7804e13cdd4b0ec
avehghasemian <av.ghasemian@gmail.com>
2021-02-04 10:10:57 +0100

    user

--- controllers/user.js
+++ controllers/user.js
@@ -14,18 +14,4 @@ exports.signup = (req, res, next) => {
         email: req.body.email,
         password: hash,
       })
-      user
-        .save()
-        .then(() => res.status(201).json({ message: 'Utilisateur créé ! ' }))
-        .catch((error) => res.status(400).json({ error }))
-    })
-    .catch((error) => res.status(500).json({ error }))
-}
-
-exports.login = (req, res, next) => {
-  // Pour la connection à son compte utilisateur
-  User.findOne({ email: req.body.email })
-    .then((user) => {
-      if (!user) {
-        return res.status(401).json({ error: 'Utilisateur non trouvé !' })
-      }
+     
