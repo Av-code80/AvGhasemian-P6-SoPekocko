992a8bbfd5c93bca36d4145d085a071b03536740
avehghasemian <av.ghasemian@gmail.com>
2021-02-04 10:08:51 +0100

    errorHandler

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const http = require('http') // On appelle l'objet 'http' de node
 const app = require('./app') // On appelle l'application que renverra le serveur
 
-
 const normalizePort = (val) => {
-    // la fonction normalizePort renvoie un port valide, qu'il soit fourni sous la forme d'un numéro ou d'une chaîne ;
-    const port = parseInt(val, 10)
-  
-    if (isNaN(port)) {
-      return val
-    }
-    if (port >= 0) {
-      return port
-    }
-    return false
+  // la fonction normalizePort renvoie un port valide, qu'il soit fourni sous la forme d'un numéro ou d'une chaîne ;
+  const port = parseInt(val, 10)
+
+  if (isNaN(port)) {
+    return val
   }
-  const port = normalizePort(process.env.PORT || '3000')
-  
+  if (port >= 0) {
+    return port
+  }
+  return false
+}
+const port = normalizePort(process.env.PORT || '3000')
+
+app.set('port', port) // On doit spécifié sur quel port tourne l'application ( port 3000 par défaut ou sur le port spécifié par l'environnement)
+
+const errorHandler = (error) => {
+  // recherche les différentes erreurs et les gère de manière appropriée. Elle est ensuite enregistrée dans le serveur ;
+  if (error.syscall !== 'listen') {
+    throw error
+  }
