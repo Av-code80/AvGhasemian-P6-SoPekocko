16bd9b08ad01ae1274569dd0abd6ed22e1fcf9ec
avehghasemian <av.ghasemian@gmail.com>
2021-02-04 10:38:59 +0100

    optimisation code

--- server.js
+++ server.js
@@ -39,4 +39,15 @@ const errorHandler = (error) => {
   }
 }
 
+
+
 const server = http.createServer(app) // On créer notre serveur grâce à le méthode '.createServer qui prend deux paramètres
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+server.listen(port) // Le serveur écoute les requêtes sur le port 3000 par défaut ou sur le port spécifié par l'environnement
